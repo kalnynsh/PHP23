7b70de96fba9472cee9fa0388d5df4efe7a00901
kalnynsh <kda869@yandex.ru>
2018-04-22 18:07:10 +0300

    Update content.

--- config/db.php
+++ config/db.php
@@ -5,33 +5,25 @@
  * 
  * PHP version 7.2
  * 
- * @category Education_PHP
- * @package  PHP_Education_DB
- * @author   Kalnynsh <kalnynsh@google.com>
+ * @category Learning_PHP
+ * @package  PHP_DB
+ * @author   Kalnynsh <kda869@yandex.ru>
  * @license  http://example.com MIT 
  * @link     https://github.com/kalnynsh 
  */
 
-/**
- * Return array of DB connection params
- *
- * @return array
- */
-function getConnParams() : array
-{
-    $host = 'localhost';
-    $user = 'd';
-    $passw = 'Mc2tu7elq';
-    $port = 3306;
-    $db_name = 'd_edu01';
+define('DB_CONNECTION', 'mysql');
+define('DB_HOST', '127.0.0.1');
+define('DB_PORT', '3306');
+define('DB_DATABASE', 'd_edu01');
+define('DB_USERNAME', 'd');
+define('DB_PASSWORD', 'Mc2tu7elq');
 
-    $connectionParams = compact(
-        'host',
-        'user',
-        'passw',
-        'port',
-        'db_name'
-    );
-
-    return $connectionParams;
-}
+return [
+    DB_CONNECTION,
+    DB_HOST,
+    DB_PORT,
+    DB_DATABASE,
+    DB_USERNAME,
+    DB_PASSWORD
+];
--- config/main.php
+++ config/main.php
@@ -2,13 +2,13 @@
 header('Content-Type: text/html; charset=utf-8');
 
 /** 
- * PHP education
+ * Learning PHP
  * 
  * PHP version 7.2
  * 
- * @category Education_PHP
- * @package  PHP_Education_Config
- * @author   Kalnynsh <kalnynsh@google.com>
+ * @category Learning_PHP
+ * @package  PHP_Config
+ * @author   Kalnynsh <kda869@yandex.ru>
  * @license  http://example.com MIT 
  * @link     https://github.com/kalnynsh 
  */
@@ -19,49 +19,14 @@ header('Content-Type: text/html; charset=utf-8');
 define('ROOT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/..');
 
 /**
- * Define constant the Path to partials
- */
-// define('PARTIALS_PATH', ROOT_DIR . '/templates/layouts/patials');
-
-/**
- * Define constant the Path to header partial
- */
-// define('HEADER_PATH', ROOT_DIR . '/templates/layouts/patials/header.php');
-
-/**
- * Define constant the Path to content partial
- */
-// define('CONTENT_PATH', ROOT_DIR . '/templates/layouts/patials/content.php');
-
-/**
- * Define constant the Path to footer partial
- */
-// define('FOOTER_PATH', ROOT_DIR . '/templates/layouts/patials/footer.php');
-
-/**
- * Define constant the Path to common data
- */
-// define('COMMON_DATA', ROOT_DIR . '/templates/layouts/common_data.php');
-
-/**
  * Define constant the Path to public/
  */
 define('PUBLIC_DIR', ROOT_DIR . '/public');
 
 /**
- * Define constant the Path to engine/
- */
-define('ENGINE_DIR', ROOT_DIR . '/engine');
-
-/**
  * Define constant the Path to uploads/
  */
 define('UPLOADS_DIR', ROOT_DIR . '/uploads');
-
-/**
- * Define constant the Path to /public/images
- */
-define('IMAGES_DIR', ROOT_DIR . '/public/images');
 
 /**
  * Define constant the Path to /templates/layouts
