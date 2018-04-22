4e447a69f226ffe09c7ee20565b8ea8fd0fc8dfb
kalnynsh <kda869@yandex.ru>
2018-04-22 19:24:47 +0300

    Refactor code.

--- services/Autoloader.php
+++ services/Autoloader.php
@@ -2,16 +2,38 @@
 
 namespace app\services;
 
+require_once $_SERVER['DOCUMENT_ROOT'] . '/..' . '/config/app.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/..' . '/config/main.php';
+
+/**
+ * Class using for autoload classes
+ */
 class Autoloader
 {
-    private $fileExtension = ".php";
+    private $_fileExtension = ".php";
 
+    /**
+     * Include class
+     *
+     * @param string $className - class name
+     * 
+     * @return bool
+     */
     public function loadClass($className)
     {
-        $className = str_replace(["app\\", "\\"], [ROOT_DIR, DS], $className);
-        $className .= $this->fileExtension;
+        $className = str_replace(
+            [APP_NAME . '\\', '\\'],
+            [ROOT_DIR, DIRECTORY_SEPARATOR],
+            $className
+        );
+        $className .= $this->_fileExtension;
+
         if (file_exists($className)) {
             include $className;
+            return true;
         }
+
+        echo 'File or class not exists';
+        return false;
     }
 }
