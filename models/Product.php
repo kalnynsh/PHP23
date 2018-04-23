c930868831c7e204f4e588c306128845b571e010
kalnynsh <kda869@yandex.ru>
2018-04-23 11:10:08 +0300

    Update properties.

--- models/Product.php
+++ models/Product.php
@@ -1,16 +1,39 @@
 <?php
 namespace app\models;
 
-
+/**
+ *  Product model class
+ */
 class Product extends Model
 {
     public $id;
-    public $name;
-    public $description;
-    public $price;
-    public $customer;
+    public $id_category;
+    public $id_image;
+    public $id_material;
 
-    public function getTableName()
+    public $price;
+    public $product_name;
+    public $size;
+
+    public $amount;
+    public $color;
+    protected $allowedProperties = [
+        'id_category',
+        'id_image',
+        'id_material',
+        'price',
+        'product_name',
+        'size',
+        'amount',
+        'color'
+    ];
+
+    /**
+     * Return DB table name
+     *
+     * @return string
+     */
+    public function getTableName() : string
     {
         return 'products';
     }
