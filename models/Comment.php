0fb6fb73c5aefc34ae8c26eca2f12d711c011e22
kalnynsh <kda869@yandex.ru>
2018-04-23 18:51:43 +0300

    Create Comment model class.

---
+++ models/Comment.php
@@ -0,0 +1,27 @@
+<?php
+namespace app\models;
+
+/**
+ *  Comment model class
+ */
+class Comment extends Model
+{
+    public $id;
+    public $user_id;
+    public $content;
+
+    protected $allowedProperties = [
+        'user_id',
+        'content',
+    ];
+
+    /**
+     * Return DB table name
+     *
+     * @return string
+     */
+    public function getTableName() : string
+    {
+        return 'comments';
+    }
+}
