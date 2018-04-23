5b0746441adec47a6acad8e0350abcc31cb7f8cb
kalnynsh <kda869@yandex.ru>
2018-04-23 11:15:14 +0300

    Add doc strings.

--- interfaces/IModel.php
+++ interfaces/IModel.php
@@ -1,11 +1,23 @@
 <?php
 namespace app\interfaces;
 
+/**
+ * Interfaces for Model
+ */
 interface IModel
 {
+    /**
+     * Get one row of data from DB by ID
+     */
     public function getOne($id);
 
+    /**
+     * Get all row data from DB
+     */
     public function getAll();
 
+    /**
+     * Get table name
+     */
     public function getTableName();
 }
