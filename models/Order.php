7733c5034b44303ee9e3ffd5a18a3f1f307c3d0c
kalnynsh <kda869@yandex.ru>
2018-04-23 19:30:18 +0300

    Create model class Order.

---
+++ models/Order.php
@@ -0,0 +1,33 @@
+<?php
+namespace app\models;
+
+/**
+ *  Order model class
+ */
+class Order extends Model
+{
+    public $id;
+    public $order_number;
+    public $product_amount;
+    public $product_id;
+    public $user_id;
+    public $created_at;
+
+    protected $allowedProperties = [
+        'order_number',
+        'product_amount',
+        'product_id',
+        'user_id',
+        'created_at',
+    ];
+
+    /**
+     * Return DB table name
+     *
+     * @return string
+     */
+    public function getTableName() : string
+    {
+        return 'orders';
+    }
+}
