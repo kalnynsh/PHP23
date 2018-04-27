3a1f7917ad0f5e3a16cdaf74126e3935b7b0bc09
kalnynsh <kda869@yandex.ru>
2018-04-27 17:19:31 +0300

    Add __constract method.

--- models/Product.php
+++ models/Product.php
@@ -28,6 +28,41 @@ class Product extends DbModel
         'amount',
         'color'
     ];
+    /**
+     * Product constructor
+     *
+     * @param int    $id           - product's ID
+     * @param int    $category_id  - product's category ID
+     * @param int    $image_id     - product's image ID
+     * @param int    $material_id  - product's material ID
+     * @param double $price        - product's price
+     * @param string $product_name - product's name
+     * @param string $size         - product's size
+     * @param int    $amount       - product's amount
+     * @param string $color        - product's color
+     */
+    public function __construct(
+        $id = null,
+        $category_id = null,
+        $image_id = null,
+        $material_id = null,
+        $price = null,
+        $product_name = null,
+        $size = null,
+        $amount = null,
+        $color = null
+    ) {
+        parent::__construct();
+        $this->id = $id;
+        $this->category_id = $category_id;
+        $this->image_id = $image_id;
+        $this->material_id = $material_id;
+        $this->price = $price;
+        $this->product_name = $product_name;
+        $this->size = $size;
+        $this->amount = $amount;
+        $this->color = $color;
+    }
 
     /**
      * Return DB table name
