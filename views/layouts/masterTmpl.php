5a225527df5bf24ff6982f98d95a4596bac397fe
kalnynsh <kda869@yandex.ru>
2018-04-28 13:10:36 +0300

    Update href links.

--- views/layouts/masterTmpl.php
+++ views/layouts/masterTmpl.php
@@ -7,14 +7,14 @@
 <meta name="description" content="education">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
-<link rel="apple-touch-icon" href="images/icon.png">
-<link rel="stylesheet" href="css/normalize.css">
-<link rel="stylesheet" href="css/app.css">
+<link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon" href="/images/icon.png">
+<link rel="stylesheet" href="/css/normalize.css">
+<link rel="stylesheet" href="/css/app.css">
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"
 integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
 crossorigin="anonymous" defer></script>
-<script src="js/app.js" defer></script>
+<script src="/js/app.js" defer></script>
 </head>
 <body>
     <div class="wrap">   
@@ -22,22 +22,22 @@ crossorigin="anonymous" defer></script>
         <nav>
         <ul class="menu1">
             <li class="menu1__item">
-                <a href="index.php">Главная</a>
+                <a href="/index.php">Главная</a>
             </li>          
             <li class="menu1__item">
-                <a href="comments.php">Оставить комментарий</a>
+                <a href="/comments/index">Оставить комментарий</a>
             </li>
             <li class="menu1__item">
-                <a href="register.php">Регистрация</a>
+                <a href="/register/index">Регистрация</a>
             </li>
             <li class="menu1__item">
-                <a href="login.php">Вход</a>
+                <a href="/login/index">Вход</a>
             </li>
             <li class="menu1__item">
-                <a href="logout.php">Выход</a>
+                <a href="/logout/index">Выход</a>
             </li>
             <li class="menu1__item">
-                <a href="cart.php">Личный кабинет</a>
+                <a href="/cart/index">Личный кабинет</a>
             </li>                                    
             <!-- <li class="menu1__item">
                 <a href="admin.php">Админ панель</a>
