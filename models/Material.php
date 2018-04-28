e5f58ad7a4e11a3436af86c3dc1dbb1e5c20aab1
kalnynsh <kda869@yandex.ru>
2018-04-28 10:44:23 +0300

    Create __constract().

--- models/Material.php
+++ models/Material.php
@@ -9,9 +9,21 @@ class Material extends DbModel
     public $id;
     public $material_name;
 
-    protected $allowedProperties = [
-        'material_name',
-    ];
+    protected $allowedProperties = [];
+
+    /**
+     * Material's constructor
+     *
+     * @param int    $id            - Material's ID
+     * @param string $material_name - Material's name
+     */
+    public function __construct(
+        $id = null,
+        $material_name = null
+    ) {
+        $this->id = $id;
+        $this->material_name = $material_name;
+    }
 
     /**
      * Return DB table name
