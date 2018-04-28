7333e32e13a311137af3ccc387cf6d7e0deb76a4
kalnynsh <kda869@yandex.ru>
2018-04-28 10:41:15 +0300

    Create __constract().

--- models/Image.php
+++ models/Image.php
@@ -11,11 +11,27 @@ class Image extends DbModel
     public $date_created;
     public $date_updated;
 
-    protected $allowedProperties = [
-        'image_name',
-        'date_created',
-        'date_updated',
-    ];
+    protected $allowedProperties = [];
+
+    /**
+     * Image's constructor
+     *
+     * @param int    $id           - Image's ID
+     * @param string $image_name   - Image's name
+     * @param string $date_created - creation date, time 
+     * @param string $date_updated - update date, time  
+     */
+    public function __construct(
+        $id = null,
+        $image_name = null,
+        $date_created = null,
+        $date_updated = null
+    ) {
+        $this->id = $id;
+        $this->image_name = $image_name;
+        $this->date_created = $date_created;
+        $this->date_updated = $date_updated;
+    }
 
     /**
      * Return DB table name
