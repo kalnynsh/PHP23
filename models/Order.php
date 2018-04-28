93e6f4eba82fc8826ca01cc442ddd4ed14bfc7b4
kalnynsh <kda869@yandex.ru>
2018-04-28 10:54:48 +0300

    Create __constract().

--- models/Order.php
+++ models/Order.php
@@ -12,14 +12,33 @@ class Order extends DbModel
     public $product_id;
     public $user_id;
     public $created_at;
+    protected $allowedProperties = [];
 
-    protected $allowedProperties = [
-        'order_number',
-        'product_amount',
-        'product_id',
-        'user_id',
-        'created_at',
-    ];
+    /**
+     * Order's constructor
+     *
+     * @param int    $id             - order's ID
+     * @param string $order_number   - order's number
+     * @param string $product_amount - product's number 
+     * @param string $product_id     - product's ID 
+     * @param string $user_id        - user's ID
+     * @param string $created_at     - creation date, time
+     */
+    public function __construct(
+        $id = null,
+        $order_number = null,
+        $product_amount = null,
+        $product_id = null,
+        $user_id = null,
+        $created_at = null
+    ) {
+        $this->id = $id;
+        $this->order_number = $order_number;
+        $this->product_amount = $product_amount;
+        $this->product_id = $product_id;
+        $this->user_id = $user_id;
+        $this->created_at = $created_at;
+    }
 
     /**
      * Return DB table name
