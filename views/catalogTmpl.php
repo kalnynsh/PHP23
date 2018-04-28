4148291f0e7b9ca43633d7ed4d159a295c07511f
kalnynsh <kda869@yandex.ru>
2018-04-28 16:43:06 +0300

    Update code using object model of product.

--- views/catalogTmpl.php
+++ views/catalogTmpl.php
@@ -1,19 +1,19 @@
 <div class="greeting">
-    <?php if ($usename) : ?>
-        Добро пожаловать <?php echo ucfirst(($usename)); ?>
+    <?php if ($username) : ?>
+        Добро пожаловать <?php echo ucfirst(($username)); ?>!
     <?php endif; ?>
 </div>
 <div class="products">
     <?php foreach ($products as $product) : ?>        
         <div class="products__item">
-            <a href="/product.php?id=<?php echo $product['product_id']; ?>">
+            <a href="?c=product&a=cart&?id=<?php echo $product->id; ?>">
                 <img width="200" 
-                    src="/images/products_small/<?php echo $product['image_name']; ?>" 
+                    src="/images/products_small/<?php echo 'item-2' . $product->image_id . '.jpg'; ?>" 
                     alt="image">
                 <div class="product-description">
-                    <p><?php echo $product['product_name']; ?></p>
-                    <p><?php echo $product['color']; ?></p>
-                    <p>Цена: <?php echo $product['price']; ?>руб.</p>
+                    <p><?php echo $product->product_name; ?></p>
+                    <p><?php echo $product->color; ?></p>
+                    <p>Цена: <?php echo $product->price; ?>руб.</p>
                 </div>
             </a>
         </div>               
