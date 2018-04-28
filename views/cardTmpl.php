352659f169623ba4b4b09793cdf8fa2ba0ababae
kalnynsh <kda869@yandex.ru>
2018-04-28 18:05:58 +0300

    Update code.

--- views/cardTmpl.php
+++ views/cardTmpl.php
@@ -3,13 +3,28 @@
         <img width="100%" 
             src="/images/products/<?php echo 'item-2' . $product->image_id . '.jpg'; ?>" 
                 alt="image">
-            <div class="product-description">
-                <p>
-                    <?php echo $product->product_name; ?>
-                    <?php echo $product->color; ?>
-                </p>
-                <p>Цена: <?php echo $product->price; ?> руб.</p>
-                <p>Количество: <?php echo $product->amount; ?> шт.</p>
-            </div>
+        <div class="product-description">
+            <p>
+                <?php echo $product->product_name; ?>
+                <?php echo $product->color; ?>
+            </p>
+            <p>Цена: <?php echo $product->price; ?> руб.</p>
+            <p>Количество: <?php echo $product->amount; ?> шт.</p>
+        </div>
+        <div>
+            <?php if ($is_login) : ?>
+                <div class="product-addtocart">                
+                        <input type="hidden" name="id" 
+                            value="<?php echo $product->id; ?>">
+                        <input type="number" name="amount" value="1" range="1-100">
+                        <input type="submit" name="submit_add_to_cart" 
+                            class="add-to-cart"                        
+                        value="Добавить в корзину">  
+                        <input type="submit" name="submit_edit_cart"
+                            class="edit_cart"
+                            value="Изменить содержимое корзины">                
+                </div>
+            <?php endif; ?>  
+        </div>
     </div>
 </div>
