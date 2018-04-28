abe19131fd24d2c9ab95de0a5c3167914da24f0c
kalnynsh <kda869@yandex.ru>
2018-04-28 18:34:32 +0300

    Add __construct().

--- models/User.php
+++ models/User.php
@@ -19,6 +19,29 @@ class User extends DbModel
     ];
 
     /**
+     * User's constractor
+     *
+     * @param int    $id         - user's ID
+     * @param string $login      - user's login
+     * @param string $name       - user's name
+     * @param string $password   - password
+     * @param string $last_login - date of las login
+     */
+    public function __construct(
+        $id = null,
+        $login = null,
+        $name = null,
+        $password = null,
+        $last_login = null
+    ) {
+        $this->id = $id;
+        $this->login = $login;
+        $this->name = $name;
+        $this->password = $password;
+        $this->last_login = $last_login;
+    }
+
+    /**
      * Get DB table name
      *
      * @return void
