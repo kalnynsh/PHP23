51476325dff25975c43d0ffbefe899feb30fa147
kalnynsh <kda869@yandex.ru>
2018-04-28 16:23:03 +0300

    Move *.css, *.jpg to public dir.

--- views/catalogTmpl.php
+++ views/catalogTmpl.php
@@ -1,12 +1,12 @@
 <div class="greeting">
     <?php if ($usename) : ?>
-        Добро пожаловать <?php echo $usename; ?>
+        Добро пожаловать <?php echo ucfirst(($usename)); ?>
     <?php endif; ?>
 </div>
 <div class="products">
     <?php foreach ($products as $product) : ?>        
         <div class="products__item">
-            <a href="/product.php?id=<?php echo $product['id_product']; ?>">                    <img width="200" 
+            <a href="/product.php?id=<?php echo $product['product_id']; ?>">                    <img width="200" 
                     src="/images/products_small/<?php echo $product['image_name']; ?>" 
                     alt="image">
                 <div class="product-description">
