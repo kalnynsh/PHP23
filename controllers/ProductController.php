ab97992ed13787ca6ccb259f6e2035a8854aaaaa
kalnynsh <kda869@yandex.ru>
2018-04-28 16:45:51 +0300

    Create class ProductController.

---
+++ controllers/ProductController.php
@@ -0,0 +1,55 @@
+<?php
+
+/** 
+ * PHP Controller
+ * 
+ * PHP version 7.2
+ * 
+ * @category Controllers
+ * @package  Product_Controller
+ * @author   Kalnynsh <kda869@yandex.ru>
+ * @license  http://example.com MIT 
+ * @link     https://github.com/kalnynsh 
+ */
+
+namespace app\controllers;
+
+use app\models\Product;
+
+/**
+ * Product Controller
+ */
+class ProductController extends Controller
+{
+    /**
+     * Default controller's action
+     *
+     * @return void
+     */
+    public function actionIndex()
+    {
+        $products = Product::getAll();
+        $username = 'anonim';
+
+        $params = [
+            'products' => $products,
+            'username' => $username,
+        ];
+
+        echo $this->render('catalog', $params);
+    }
+
+    /**
+     * Cart action
+     *
+     * @return void
+     */
+    public function actionCart()
+    {
+        $id = filter_var(intval($_GET['id']), FILTER_VALIDATE_INT);
+        $product = Product::getOne($id);
+
+        echo $this->render('cartSimple', ['product' => $product]);
+    }
+
+}
